9e59acb48a7ead211b5c0a83e1641f915eddaf8d
markitos2001 <marcosrequenagut@gmail.com>
2025-03-28 19:59:57 +0100

    Add new test

--- tests/integration_test/test_utils_with_sqlite.py
+++ tests/integration_test/test_utils_with_sqlite.py
@@ -1,11 +1,32 @@
-
+import sqlite3
+from tempfile import TemporaryDirectory
+from pathlib import Path
 
 def setup():
-    # Prepare the environement
+    """ This test requires:
+    - There is a database we can connect to
+    - The database has the required tables created
+    - There is data in the tables
+    """
+    #directory = TemporaryDirectory()
+    folder = Path("./test_data")
+    folder.mkdir(exist_ok=True)
+    with sqlite3.connect(folder/"project_database.db") as connection:
+        cursor = connection.cursor()
+
+        cursor.execute("""
+CREATE TABLE IF NOT EXISTS Project (
+    name TEXT PRIMARY KEY,
+    time INTEGER
+)
+""")    
+        cursor.execute("INSERT INTO Project (name, time) VALUES ( ?, ?)", ("project1", 0))
+        cursor.execute("INSERT INTO Project (name, time) VALUES ( ?, ?)", ("project2", 95))
+        connection.commit()
+
+
+
+def test_check_if_project_exists():
     pass
 
-def tear_down():
-    # Clean up after tests
-    pass
-
-def check_if_project_exists()
+setup()
--- tests/unit_test/test_models.py
+++ tests/unit_test/test_models.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.crud_api.models import Project
+from crud_api.models import Project
 from pydantic import ValidationError
 
 def test_time_cant_be_negative():
